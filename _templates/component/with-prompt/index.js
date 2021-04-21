1e331543de34e4f0a5ad44a6e03e30c3585a8691
hhhtak <matuyamen@hotmail.co.jp>
2021-04-22 08:36:11 +0900

    downgrade package.json

--- _templates/component/with-prompt/index.js
+++ _templates/component/with-prompt/index.js
@@ -1,16 +1,34 @@
 // see types of prompts:
 // https://github.com/enquirer/enquirer/tree/master/examples
 //
-module.exports = [
-  {
-    type: "input",
-    name: "message",
-    message: "What's your message?",
-    choices: ["standard", "organism", "moment"],
-  },
-  {
-    type: "input",
-    name: "name",
-    message: "What's your hogehoge?",
-  },
-];
+module.exports = {
+  prompt: ({ inquirer }) =>
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "message",
+        message: "What's your message?",
+        choices: ["aaa", "bbb", "ccc"],
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "What's your messageeeee?",
+      },
+    ]),
+  // .then(({ message }) => inquirer.prompt()),
+};
+
+// module.exports = [
+//   {
+//     type: "list",
+//     name: "message",
+//     message: "What's your message?",
+//     choices: ["aaa", "bbb", "ccc"],
+//   },
+//   {
+//     type: "input",
+//     name: "name",
+//     message: "What's your messageeeee?",
+//   },
+// ];
